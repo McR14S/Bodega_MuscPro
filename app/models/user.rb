b4471d5e546f3d2559b10d7b6793df918e45ae7f
McR14S <dan.arancibiar@duocuc.cl>
2023-05-26 18:27:09 -0400

    Users Create

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+    has_secure_password
+
+    validates :email, presence: true, uniqueness: true
+    validates :username, presence: true, uniqueness: true
+    validates :password_digest, length: {minimum:6} 
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   resources :categories, except: :show
   #refactor
   resources :products, path: '/'
+
+  #reestructura el codigo como si fueran carpetas
+  namespace :authentication do
+    resources :users, only: [:new, :create]
+  end
 end
