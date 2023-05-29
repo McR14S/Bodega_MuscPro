2b0f3edff4b02406df4c0ea62e43a8449b4c0f2a
McR14S <dan.arancibiar@duocuc.cl>
2023-05-29 17:56:39 -0400

    Protect pages

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,6 @@
 class ProductsController < ApplicationController
+    skip_before_action :protect_pages, only: [:index, :show]
+
     def index
         @categories = Category.order(name: :asc).load_async
         
