89ed113320580007911f758480a34c5b52987993
McR14S <dan.arancibiar@duocuc.cl>
2023-05-31 21:43:01 -0400

    Refactor Turbo

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -65,11 +65,11 @@ class ProductsController < ApplicationController
 
     #Limpiamos parametros no especificados
     def product_params_index
-        params.permit(:category_id, :min_price, :max_price, :query_text, :order_by, :page, :favorites)
+        params.permit(:category_id, :min_price, :max_price, :query_text, :order_by, :page, :favorites, :user_id)
     end
 
     def product
-        @product = Product.find(params[:id])
+        @product ||= Product.find(params[:id])
     end
 
 end
